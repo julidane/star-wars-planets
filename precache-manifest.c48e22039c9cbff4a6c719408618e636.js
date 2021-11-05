9ce45349d3ef5ed1931e4755ffa9229f09fa900c
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-11-05 16:27:12 -0300

    Updates

---
+++ precache-manifest.c48e22039c9cbff4a6c719408618e636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e38190a7ca6eca2b7c21afbe6b79352",
+    "url": "/star-wars-planets/index.html"
+  },
+  {
+    "revision": "6ba1f4e9fe1a119b87c3",
+    "url": "/star-wars-planets/static/css/main.642b24e0.chunk.css"
+  },
+  {
+    "revision": "c5b8a1aa590ffd8c2866",
+    "url": "/star-wars-planets/static/js/2.b350eca9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-planets/static/js/2.b350eca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ba1f4e9fe1a119b87c3",
+    "url": "/star-wars-planets/static/js/main.a5bf8622.chunk.js"
+  },
+  {
+    "revision": "089944949f54aeb36a92",
+    "url": "/star-wars-planets/static/js/runtime-main.d3fb0412.js"
+  }
+]);
